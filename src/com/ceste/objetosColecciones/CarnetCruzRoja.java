f4c281384330a9fefbf29094c85a66e321382024
Mayorian <mayorianloriana23@gmail.com>
2016-04-25 14:17:50 +0200

    Empezamos la practica de objetos llamada practica_objetos_colecciones

---
+++ src/com/ceste/objetosColecciones/CarnetCruzRoja.java
@@ -0,0 +1,9 @@
+package com.ceste.objetosColecciones;
+
+/**
+ * Created by Mayorian on 25/04/2016.
+ */
+public class CarnetCruzRoja {
+
+
+}
---
+++ src/com/ceste/objetosColecciones/Main.java
@@ -0,0 +1,11 @@
+package com.ceste.objetosColecciones;
+
+/**
+ * Created by Mayorian on 25/04/2016.
+ */
+public class Main {
+    public static void main (String [] args){
+        CarnetCruzRoja carnet = new CarnetCruzRoja();
+
+    }
+}
