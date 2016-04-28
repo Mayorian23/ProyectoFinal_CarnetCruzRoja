b37dcae873d52d1ba4679a0f1f61ae23fdfb400d
Mayorian <mayorianloriana23@gmail.com>
2016-04-28 13:20:42 +0200

    se ha creado el constructor y los set con los get

--- src/com/ceste/objetosColecciones/CarnetCruzRoja.java
+++ src/com/ceste/objetosColecciones/CarnetCruzRoja.java
@@ -5,5 +5,66 @@ package com.ceste.objetosColecciones;
  */
 public class CarnetCruzRoja {
 
+   private String Nombre;
+   private String Apellidos;
+   private String DNI;
+   private String Provincia;
+   private String Localidad;
+   private String Servicio;
+   private String Fecha_caducidad;
 
+    public CarnetCruzRoja(String DNI){
+        this.DNI = DNI;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        Apellidos = apellidos;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setProvincia(String provincia) {
+        Provincia = provincia;
+    }
+
+    public String getProvincia() {
+        return Provincia;
+    }
+
+    public void setLocalidad(String localidad) {
+        Localidad = localidad;
+    }
+
+    public String getLocalidad() {
+        return Localidad;
+    }
+
+    public void setServicio(String servicio) {
+        Servicio = servicio;
+    }
+
+    public String getServicio() {
+        return Servicio;
+    }
+
+    public void setFecha_caducidad(String fecha_caducidad) {
+        Fecha_caducidad = fecha_caducidad;
+    }
+
+    public String getFecha_caducidad() {
+        return Fecha_caducidad;
+    }
 }
--- src/com/ceste/objetosColecciones/Main.java
+++ src/com/ceste/objetosColecciones/Main.java
@@ -1,11 +1,17 @@
 package com.ceste.objetosColecciones;
-
 /**
  * Created by Mayorian on 25/04/2016.
  */
 public class Main {
     public static void main (String [] args){
-        CarnetCruzRoja carnet = new CarnetCruzRoja();
+        CarnetCruzRoja nombre = new CarnetCruzRoja(" ");
+        System.out.println(nombre.toString());
+
+        CarnetCruzRoja carnet1 = new CarnetCruzRoja("String DNI");
+        CarnetCruzRoja carnet2 = new CarnetCruzRoja("String DNI");
+        CarnetCruzRoja carnet3 = new CarnetCruzRoja("String DNI");
+        CarnetCruzRoja carnet4 = new CarnetCruzRoja("String DNI");
+        CarnetCruzRoja carnet5 = new CarnetCruzRoja("String DNI");
 
     }
 }
