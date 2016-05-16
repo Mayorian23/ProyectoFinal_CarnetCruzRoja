44dcfc9b9625fb15acfdbf99c3e871b425c31b3a
Mayorian <mayorianloriana23@gmail.com>
2016-05-16 23:51:18 +0200

    he creado la clase CarnetsDb

---
+++ src/com/ceste/objetosColecciones/CarnetsDb.java
@@ -0,0 +1,33 @@
+package com.ceste.objetosColecciones;
+
+/**
+ * Created by Mayorian on 16/05/2016.
+ */
+public class CarnetsDb {
+
+    private ArrayList<CarnetCruzRoja> carnets; // Colección de carnets
+    private String fichero ; // nombre de fichero donde se guardan los carnets
+    CarnetsCurzRojaDb(String fichero) {
+
+
+
+    }
+    /**
+     * Abre el fichero en modo lectura y carga sus datos en la variable carnets
+     * Debes usar flujos de objetos (ObjectInputStream)
+     */
+
+    public void cargar() {
+
+
+    }
+    /**
+     * Abre el fichero en modo escritura y vuelca en el contenido del atributo carnets
+     * Debes usar flujos de objetos (ObjectOutputStream)
+     */
+
+    public void guardar() {
+
+
+    }
+}
